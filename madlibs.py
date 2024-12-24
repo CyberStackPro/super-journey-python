5e4519c021e1a171b2b3f5df76f3943077d9d0d1
CyberStackPro <yeabsragebriel@gmail.com>
2024-12-24 10:48:17 +0300

    create first madlib exersise

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# youtuber = 'john'
+
+# print("Hello, " + youtuber + ", welcome to my channel!")
+# print("subscribe to {}".format(youtuber))
+# print(f'subscribe to {youtuber}')
+
+adj =input('Adjective: ')
+verb1 = input('Verb: ')
+verb2  = input('Verb: ')
+famous_person = input('famous_person: ')
+madlib = f'Programming is do {adj}! It makes me so execited all the time because \
+        Ilove to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!'
+
+print(madlib)
