7b99814b9c162814502e588a42738ca54843c370
CyberStackPro <yeabsragebriel@gmail.com>
2024-12-25 11:36:07 +0300

    add number guessing

--- sample_madlibs/random_madlibs.py
+++ sample_madlibs/random_madlibs.py
@@ -2,5 +2,5 @@ from madlibs_files import code, hp, hungergames, zombi
 import random
 
 if __name__ == "__name__":
-        m = random.choice([hp, code, zombi, hungergames])
-        m.madlib()
+    m = random.choice([hp, code, zombi, hungergames])
+    m.madlib()
