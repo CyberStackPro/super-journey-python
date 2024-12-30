1abcec293448d9a48d88caa596d622fd7e2e26ff
CyberStackPro <yeabsragebriel@gmail.com>
2024-12-30 09:23:02 +0300

    Add tuples

---
+++ python_basic/tuples.py
@@ -0,0 +1,8 @@
+# Tuples
+# They are like read-only lists. We use them to store a list of items. But once we
+# define a tuple, we cannot add or remove items or change the existing items.
+
+coordinates  =(1,2,3)
+
+#  we can unpack a list or tuple into separate variables:
+x, y, z = coordinates
