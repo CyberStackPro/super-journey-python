e51d1f8b43ad59ba724f8aa4309bc455fe810806
CyberStackPro <yeabsragebriel@gmail.com>
2024-12-30 09:11:14 +0300

    Add if_else_statements

---
+++ python_basic/if_statements.py
@@ -0,0 +1,19 @@
+# If Statements
+
+if is_hot:
+    print('hot day')
+elif is_cold:
+    print('cold day')
+else:
+    print('beautiful day')
+
+
+# Logical operators:
+
+if has_high_income and has_good_credit:
+    ...
+if has_high_income or has_good_credit:
+    ...
+
+is_day = True
+is_night = not is_day
