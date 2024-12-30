19f166e775b9efd7f43ffd321df7ffd9a74fa199
CyberStackPro <yeabsragebriel@gmail.com>
2024-12-30 09:15:04 +0300

    Add loops & operators

---
+++ python_basic/comparision_operators.py
@@ -0,0 +1,6 @@
+# a > b
+# a >= b (greater than or equal to)
+# a < b
+# a <= b
+# a == b (equals)
+# a != b (not equals)
---
+++ python_basic/loops.py
@@ -0,0 +1,13 @@
+# While Loops
+i =1
+while i < 5:
+    print(i)
+    i+=1
+
+# For Loops
+for i in range(1,5):
+    print(i)
+
+# • range(5): generates 0, 1, 2, 3, 4
+# • range(1, 5): generates 1, 2, 3, 4
+# • range(1, 5, 2): generates 1, 3
